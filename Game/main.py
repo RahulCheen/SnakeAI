173a7fe44eac413fbb801069b2253c1cbc077d58
Jordan Hendriksen <jordan.hendriksen@gmail.com>
2023-04-19 22:29:07 -0500

    adjust frame rate constant movement speed

--- Game/main.py
+++ Game/main.py
@@ -15,26 +15,27 @@ def red_to_blue(steps):
     return gradient_colors
 
 class MainWindow(QMainWindow):
-    def __init__(self, game_width, game_height):
+    def __init__(self, game_width, game_height, step_time_ms):
         super().__init__()
 
         self.setWindowTitle("Snake!")
         self.setFixedSize(game_width, game_height)
 
-        self.game_widget = GameWidget(self, game_width, game_height)
+        self.game_widget = GameWidget(self, game_width, game_height, step_time_ms)
         self.setCentralWidget(self.game_widget)
 
     def keyPressEvent(self, event):
         self.game_widget.handleKeyPress(event)
 
 class GameWidget(QWidget):
-    def __init__(self, parent, game_width, game_height):
+    def __init__(self, parent, game_width, game_height, step_time_ms):
         super().__init__(parent)
+        self.step_time_ms = step_time_ms
 
         self.setFixedSize(game_width, game_height)
         self.setFocusPolicy(Qt.StrongFocus)
 
-        self.snake = Snake()
+        self.snake = Snake(self.step_time_ms)
         self.food = Food()
 
         self.key_pressed = None
@@ -44,7 +45,7 @@ class GameWidget(QWidget):
 
     def startGame(self):
         self.snake.reset()
-        self.timer.start(40)
+        self.timer.start(self.step_time_ms)
 
     def handleKeyPress(self, event):
         self.key_pressed = event.key()
@@ -137,11 +138,11 @@ class GameWidget(QWidget):
             self.parent().close()
 
     def resetGame(self):
-        self.snake = Snake()
+        self.snake = Snake(self.step_time_ms)
         self.food = Food()
 
 if __name__ == "__main__":
     app = QApplication([])
-    main_window = MainWindow(1280, 720)
+    main_window = MainWindow(1280, 720, step_time_ms=10)
     main_window.show()
     app.exec_()
--- Game/snake.py
+++ Game/snake.py
@@ -1,26 +1,44 @@
 from PyQt5.QtCore import Qt, QPoint, QRect
 
 class Snake():
-    def __init__(self):
+    def __init__(self, step_time_ms):
+        self.step_time_ms = step_time_ms
+        self.movement_step = int(step_time_ms/2)
+
         self.body = [QRect(100,100, 20, 20),
                      QRect(80,100, 20, 20),
                      QRect(60,100, 20, 20)]
-        self.direction = QPoint(20, 0)
+        self.direction = QPoint(self.movement_step, 0)
+
+        self.segment_directions = {}
+        for k, seg in enumerate(self.body):
+            self.segment_directions[f"segment_{k}"] = {
+                "direction": self.direction,
+                "movement_counter": 20/self.movement_step,
+            }
 
     def move(self):
         new_head = self.body[0].translated(self.direction)
         self.body.insert(0, new_head)
         self.body.pop()
+        # What is needed - method of linking the current direction of a segment to the segment
+        # so - change direction - head immediately changes direction and maintains direciton* for 20/movement_step
+        # movements. This stops overlap. after 20/movement_step movments the second segment will begin moving in direction*,
+        # and so on. this maintians teh snakeish method of movement characteristic of the game.
 
     def changeDirection(self, key):
-        if key == Qt.Key_Up and self.direction != QPoint(0, 20):
-            self.direction = QPoint(0, -20)
-        elif key == Qt.Key_Right and self.direction != QPoint(-20, 0):
-            self.direction = QPoint(20, 0)
-        elif key == Qt.Key_Down and self.direction != QPoint(0, -20):
-            self.direction = QPoint(0, 20)
-        elif key == Qt.Key_Left and self.direction != QPoint(20, 0):
-            self.direction = QPoint(-20, 0)
+        prev_direction = self.direction
+        if key == Qt.Key_Up and self.direction != QPoint(0, self.movement_step):
+            self.direction = QPoint(0, -self.movement_step)
+        elif key == Qt.Key_Right and self.direction != QPoint(-self.movement_step, 0):
+            self.direction = QPoint(self.movement_step, 0)
+        elif key == Qt.Key_Down and self.direction != QPoint(0, -self.movement_step):
+            self.direction = QPoint(0, self.movement_step)
+        elif key == Qt.Key_Left and self.direction != QPoint(self.movement_step, 0):
+            self.direction = QPoint(-self.movement_step, 0)
+        if prev_direction!= self.direction:
+            self.changed_direction = True
+            print(self.segment_directions)
 
     def grow(self):
         last_segment = self.body[-1]
@@ -31,7 +49,14 @@ class Snake():
         return self.body[0] in self.body[1:]
 
     def isCollidingWithFood(self, food):
-        return self.body[0] == food.rect
+        head = self.body[0]
+        head_x = head.x() + head.width()/2
+        head_y = head.y() + head.height()/2
+
+        y_in = head_y >= food.rect.y() and head_y <= food.rect.y() + food.rect.height()
+        x_in = head_x >= food.rect.x() and head_x <= food.rect.x() + food.rect.width()
+
+        return x_in and y_in
     
     def isOutOfBounds(self, width, height):
         head = self.body[0]
@@ -41,4 +66,4 @@ class Snake():
         self.body = [QRect(100,100, 20, 20),
                      QRect(80,100, 20, 20),
                      QRect(60,100, 20, 20)]
-        self.direction = QPoint(20, 0)
+        self.direction = QPoint(self.movement_step, 0)
